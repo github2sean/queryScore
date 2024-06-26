0eae1e38625a963395514c2ee1a8db11e1dbda1b
sean <seanzq0331@163.com>
2024-06-26 19:44:07 +0800

    第一版

---
+++ .history/students_20240626153359.py
@@ -0,0 +1,32 @@
+class Students:
+  bmh=''
+  zkzh=''
+  sfzh=''
+  verify=''
+  xm=''
+  classNum=''
+  yw=0
+  xx=0
+  yy=0
+  wl=0
+  hx=0
+  sw=0
+  ddyfz=0
+  ls=0
+  dl=0
+  tyyjk=0
+  fjf=0
+  zf=0
+
+  def init(self,classNum,xm,bmh,zkzh,sfzh):
+    self.classNum=classNum
+    self.xm=xm
+    self.bmh=bmh
+    self.zkzh=zkzh
+    self.sfzh=sfzh
+
+  def __str__(self):
+    return f'Students(classNum:{self.classNum},xm:{self.xm},bmh:{self.bmh},zkzh:{self.zkzh},sfzh:{self.sfzh})'
+  
+  def keys(self):
+    return ('classNum','xm','bmh','zkzh','sfzh','yw','sx','yy','wl','hx','sw','dd')
---
+++ .history/test_20240626152713.py
@@ -0,0 +1,18 @@
+'''
+Author: sean seanzq0331@163.com
+Date: 2024-06-25 12:08:07
+LastEditors: sean seanzq0331@163.com
+LastEditTime: 2024-06-26 15:26:57
+FilePath: /queryScore/test.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
+import openpyxl
+from Students import Students
+sheetTitle = ['班级','姓名','报名号','准考证号','身份证号','语文','数学',英语,物理,化学,生物,道德与法治,历史,地理,体育与健康,加分,总分']
+
+result = openpyxl.Workbook()
+sheet_result = result.active
+sheet_result.append(sheetTitle)
+result.save('中考成绩查询结果.xlsx')
+
+
